789daa5aaf5cb36b265870972d2fa18f874b5892
hhfernald <42102915+hhfernald@users.noreply.github.com>
2018-08-08 23:26:34 -0400

    Update utils.py

--- ipynotes/utils.py
+++ ipynotes/utils.py
@@ -5,14 +5,6 @@ import locale
 def get_file_text(path):
     """Return contents of file at the given path."""
     
-    #try:
-        #with open(path, encoding='utf-8') as f:
-            #text = f.read()
-    #except UnicodeDecodeError:
-        #with open(path, encoding='latin-1') as f:
-            #text = f.read().decode('string_escape')
-        #print("NOTICE: Had to use Latin-1 encoding to read file:")
-        #print("---", path)
     with open(path, mode='rb') as f:
         b = f.read()
     try:
